481def44f66e880e614904bae3aefaa2aa8e904b
messatrt <essaessa9068@gmail.com>
2024-06-15 18:27:12 +0530

    initialised git

---
+++ cmd/web/handler.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"strconv"
+)
+
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		app.notFound(w) // Use the notFound() helper
+		return
+	}
+	files := []string{
+		"./ui/html/base.html",
+		"./ui/html/partials/nav.html",
+		"./ui/html/pages/home.html",
+	}
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, err) // Use the serverError() helper.
+		return
+	}
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		app.serverError(w, err) // Use the serverError() helper.
+	}
+}
+func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		app.notFound(w) // Use the notFound() helper.
+		return
+	}
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+}
+func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		app.clientError(w, http.StatusMethodNotAllowed) // Use the clientError() helper.
+		return
+	}
+	w.Write([]byte("Create a new snippet..."))
+}
+
---
+++ cmd/web/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"database/sql"
+	"flag"
+	"log"
+	"net/http"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type application struct {
+	infoLog  *log.Logger
+	errorLog *log.Logger
+}
+
+func main() {
+
+	//addr is a command line argument
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "web:essa28=1@/snippetbox?parseTime=true", "SQL")
+	flag.Parse()
+
+	//leled loggig
+	//infoLog is used to reperesent information about the programm eg:log.printf("starting in :40000")
+	infoLog := log.New(os.Stdout, " INFO ", log.Ldate|log.Ltime)
+
+	//errorLog is used to show errors log.Lshortfile shows the file name and the line number
+	errorLog := log.New(os.Stderr, "ERROR", log.Ldate|log.Ltime|log.Lshortfile)
+
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
+
+	app := &application{
+		infoLog:  infoLog,
+		errorLog: errorLog,
+	}
+
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  app.routes(),
+	}
+	app.infoLog.Printf("Starting server on %s", *addr)
+	err = srv.ListenAndServe()
+	app.errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
